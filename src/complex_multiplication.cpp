83080a7917fff46299d863355b1aeeb1b4c0c8de
PatriotRossii <patriotrossii2019@mail.ru>
2023-05-20 20:19:38 +0300

    Upload driver and samples

--- src/complex_multiplication.cpp
+++ src/complex_multiplication.cpp
@@ -1,17 +1,52 @@
 #include <immintrin.h>
-#include <stdio.h>
+#include <iostream>
 
-void complex_multiplication(__m256 vec1, __m256 vec2, __m256 *add, __m256 *sub, __m256 *mul, __m256 *div) {
-	*add = _mm256_add_ps(vec1, vec2);
-	*sub = _mm256_sub_ps(vec1, vec2);
-	*mul = _mm256_mul_ps(vec1, vec2);
-	*div = _mm256_div_ps(vec1, vec2);
+extern "C" void complex_multiplication_(volatile __m256 *vec1, volatile __m256 *vec2, __m256 *add, __m256 *sub, __m256 *mul, __m256 *div);
+
+static void OutputVector(
+  const char* msg,
+  volatile __m256 *array
+) {
+  std::cout << msg;
+  for (int i = 0; i != 8; ++i) {
+    std::cout << (*array)[i] << ' ';
+  }
+  std::cout << '\n';
+}
+
+static void PrintResult(
+	const char* msg,
+  volatile __m256 *b, volatile __m256 *a,
+  __m256 *add, __m256 *sub, __m256 *mul, __m256 *div
+) {
+  std::cout << msg << '\n';
+
+  OutputVector("a = ", a);
+  OutputVector("b = ", b);
+
+  std::cout << '\n';
+
+  OutputVector("a + b = ", add);
+  OutputVector("a - b = ", sub);
+  OutputVector("a * b = ", mul);
+  OutputVector("a / b = ", div);
+
+  std::cout << '\n' << std::endl;
 }
 
 int main() {
-  volatile __m256 evens = _mm256_setr_ps(2.0, 4.0, 6.0, 8.0, 10.0, 12.0, 14.0, 16.0);
-  volatile __m256 odds = _mm256_setr_ps(1.0, 3.0, 5.0, 7.0, 9.0, 11.0, 13.0, 15.0);
-
-  __m256 add, sub, mul, div;
-  complex_multiplication(evens, odds, &add, &sub, &mul, &div);
+  {
+    volatile __m256 evens = _mm256_setr_ps(2.0, 4.0, 6.0, 8.0, 10.0, 12.0, 14.0, 16.0);
+    volatile __m256 odds = _mm256_setr_ps(1.0, 3.0, 5.0, 7.0, 9.0, 11.0, 13.0, 15.0);
+    __m256 add, sub, mul, div;
+    complex_multiplication_(&odds, &evens, &add, &sub, &mul, &div);
+    PrintResult("Пример 1", &odds, &evens, &add, &sub, &mul, &div);
+  }
+  {
+    volatile __m256 vec1 = _mm256_setr_ps(75.0, 24.0, 51.0, 19.0, 48.0, 11.0, 11.0, 75.0);
+    volatile __m256 vec2 = _mm256_setr_ps(27.0, 62.0, 5.0, 83.0, 55.0, 25.0, 46.0, 69.0);
+    __m256 add, sub, mul, div;
+    complex_multiplication_(&vec2, &vec1, &add, &sub, &mul, &div);
+    PrintResult("Пример 2", &vec2, &vec1, &add, &sub, &mul, &div);
+  }
 }
