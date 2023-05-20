93fa97d0c34e1b61e6adc9480bac950e3d3fb163
PatriotRossii <patriotrossii2019@mail.ru>
2023-05-20 19:35:17 +0300

    Add volatile qualifiers to escape optimizing away

--- src/complex_multiplication.cpp
+++ src/complex_multiplication.cpp
@@ -9,16 +9,9 @@ void complex_multiplication(__m256 vec1, __m256 vec2, __m256 *add, __m256 *sub, 
 }
 
 int main() {
-  __m256 evens = _mm256_setr_ps(2.0, 4.0, 6.0, 8.0, 10.0, 12.0, 14.0, 16.0);
-  __m256 odds = _mm256_setr_ps(1.0, 3.0, 5.0, 7.0, 9.0, 11.0, 13.0, 15.0);
+  volatile __m256 evens = _mm256_setr_ps(2.0, 4.0, 6.0, 8.0, 10.0, 12.0, 14.0, 16.0);
+  volatile __m256 odds = _mm256_setr_ps(1.0, 3.0, 5.0, 7.0, 9.0, 11.0, 13.0, 15.0);
 
   __m256 add, sub, mul, div;
   complex_multiplication(evens, odds, &add, &sub, &mul, &div);
-
-  for (int i = 0; i != 8; ++i) {
-  	printf(
-  		"add = %f, sub = %f, mul = %f, div = %f\n",
-  		((float*)&add)[i], ((float*)&sub)[i], ((float*)&mul)[i], ((float*)&div)[i]
-  	);
-  }
 }
